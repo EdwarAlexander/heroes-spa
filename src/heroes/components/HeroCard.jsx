5af83b3099d94138a6835e4b474b847eeba2ee4d
Edwar Moran <edwar.moran.12@gmail.com>
2024-06-27 19:53:59 -0500

    ajuste de tarjeta de heroe

--- src/heroes/components/HeroCard.jsx
+++ src/heroes/components/HeroCard.jsx
@@ -11,20 +11,23 @@ export const HeroCard = ({
 
      const heroImageUrl = `/assets/heroes/${ id }.jpg`;
 
-  return (
-    <div className="col">
-        <div className="card">
-            <div className="row no-gutters">
-                <img src={ heroImageUrl } alt={superhero} className="card-image" />
-            </div>
-            <div className="col-8">
-                <div className="card-body">
-                    <h5 className="card-title">{ superhero }</h5>
-                    <p className="card-text">{ alter_ego }</p>
-                    <p>{ characters }</p>
+    return (
+        <div className="col">
+            <div className="card">
+                <div className="row no-gutters">
+                    <div className="col-4">
+                        <img src={ heroImageUrl } alt={superhero} className="card-img" />
+                    </div>
+                    <div className="col-8">
+                        <div className="card-body">
+                            <h5 className="card-title">{ superhero }</h5>
+                            <p className="card-text">{ alter_ego }</p>
+                            <p>{ characters }</p>
+                        </div>
+                    </div>
                 </div>
+                
             </div>
         </div>
-    </div>
-  )
+    )
 }
