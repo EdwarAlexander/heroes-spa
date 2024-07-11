42b69d6777e36b3f9ad8f66716d5d283edecd22a
Edwar Moran <edwar.moran.12@gmail.com>
2024-07-10 19:25:29 -0500

    uso de useMemo para la carga de informacion

--- src/heroes/pages/HeroPage.jsx
+++ src/heroes/pages/HeroPage.jsx
@@ -1,15 +1,53 @@
-import { useParams } from 'react-router-dom';
+import { useMemo } from 'react';
+import { Navigate, useNavigate, useParams } from 'react-router-dom';
 import { getHeroById } from '../helpers';
 
 export const HeroPage = () => {
 
   const {id} = useParams();
-  const hero = getHeroById(id);
-  console.log(hero)
+  const navigate = useNavigate();
+
+  const hero = useMemo( () => getHeroById(id) , [id]);
+  
+  if (!hero) {
+    return <Navigate to={"/marvel"} />
+  }
+
+  const OnNavigateBack = () => {
+    navigate(-1);
+  }
 
   return (
-    <>
-        <h1>HeroPage</h1>
-    </>
+    <div className="row mt-5">
+      <div className="col-4">
+        <img 
+          src={`/assets/heroes/${ id }.jpg`} 
+          alt={ hero.superhero }
+          className="img-thumbnail"
+        />
+      </div>
+      <div className="col-8">
+        <h3>{ hero.superhero }</h3>
+        <ul className="list-group list-group-flush">
+          <div className="list-group-item">
+            <b>Alter ego:</b> { hero.alter_ego }
+          </div>
+          <div className="list-group-item">
+            <b>Publisher:</b> { hero.publisher }
+          </div>
+          <div className="list-group-item">
+            <b>First appearance:</b> { hero.first_appearance }
+          </div>
+        </ul>
+        <h5 className="mt-3">Characters</h5>
+        <p>{ hero.characters }</p>
+        <button
+          className="btn btn-outline-primary"
+          onClick={ OnNavigateBack }
+        >
+          Go Back
+        </button>
+      </div>
+    </div>
   )
 }
