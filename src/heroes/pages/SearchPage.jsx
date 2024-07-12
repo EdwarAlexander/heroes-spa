55fb67b7d5ab9779cfe2146454ab7005cc521773
Edwar Moran <edwar.moran.12@gmail.com>
2024-07-11 19:55:48 -0500

    se agrego funcionalidad de busqueda

--- src/heroes/pages/SearchPage.jsx
+++ src/heroes/pages/SearchPage.jsx
@@ -1,5 +1,31 @@
+import { useLocation, useNavigate } from 'react-router-dom';
+import queryString from 'query-string';
+import { useForm } from '../../hooks/useForm';
+import { getHeroesByName } from '../helpers';
+import { HeroCard } from '../components/HeroCard';
 
 export const SearchPage = () => {
+
+  const navigate = useNavigate();
+  const location = useLocation();
+
+  const { q = '' } = queryString.parse( location.search );
+
+  const heroes = getHeroesByName( q );
+
+  const { searchText, onInputChange } = useForm({
+    searchText: q
+  });
+
+  const onSearchSubmit = (event) => {
+    event.preventDefault();
+
+    if(searchText.trim().length <= 1) return;
+
+    navigate(`?q=${ searchText.toLowerCase().trim() }`);
+    
+  }
+
   return (
     <>
         <h1>SearchPage</h1>
@@ -8,13 +34,15 @@ export const SearchPage = () => {
           <div className="col-5">
             <h4>Searching</h4>
             <hr />
-            <form>
+            <form onSubmit={ onSearchSubmit }>
               <input 
                 type="text" 
                 placeholder="Search a hero"
                 className="form-control"
                 name="searchText"
                 autoComplete="off"
+                value={ searchText }
+                onChange={ onInputChange }
               />
               <button
                 className="btn btn-outline-primary mt-2"
@@ -30,8 +58,13 @@ export const SearchPage = () => {
               Search a hero
             </div>
             <div className="alert alert-danger">
-              No hero with <b>ABC</b>
+              No hero with <b>{ q }</b>
             </div>
+            {
+              heroes.map( hero => (
+                <HeroCard key={ hero.id } { ...hero } />
+              ))
+            }
           </div>
         </div>
         
